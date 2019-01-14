4756bbe716d1fa5236d908e81a33c49d01e2c0d2
Token 414 <token414@gmail.com>
2019-01-14 21:56:35 +0100

    First Wallet Function

--- FirstPlutusSmartContract.hs
+++ FirstPlutusSmartContract.hs
@@ -2,7 +2,18 @@ module MyFirstPlutusSmartContract where
 
 import qualified Language.PlutusTx            as PlutusTx
 import           Ledger
+import           Wallet
+import           Playground.Contract
+
 
 myFirstValidator :: ValidatorScript
 myFirstValidator = ValidatorScript (fromCompiledCode $$(PlutusTx.compile
     [|| \(a :: ()) (b :: ()) (c :: ()) -> ()  ||]))
+   
+smartContractAddress :: Address'
+smartContractAddress = scriptAddress myFirstValidator
+
+watchSmartContract :: MockWallet ()
+watchSmartContract = startWatching smartContractAddress
+
+$(mkFunction 'watchSmartContract)
